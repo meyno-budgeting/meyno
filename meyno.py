e3f4dc7d777982e57b7ccf599ea9bde68545ba5e
ChaoticDefense <supersonicseven@gmail.com>
2025-07-11 16:21:33 -0500

    wrapped the script in a function for now. Started working on basic choices for functionality

--- meyno.py
+++ meyno.py
@@ -8,23 +8,67 @@ from pathlib import Path
 # Global Variables
 SCRIPT_DIR = Path(__file__).parent
 
-# Makes a database called budget.meyno (if it doesn't exist) and connects to it
-con = sqlite3.connect("budget.meyno")
+def meyno_basic():
+    # Makes a database called budget.meyno (if it doesn't exist) and connects to it
+    con = sqlite3.connect("budget.meyno")
 
-# Make a cursor to actually interact with database
-cur = con.cursor()
+    # Make a cursor to actually interact with database
+    cur = con.cursor()
 
-# For now, just make an "expenses" table (will split out into account creation later)
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS expenses(
-        id,
-        date,
-        payee,
-        amount,
-        memo
-        )
-    """)
+    # For now, just make an "expenses" table (will split out into account creation later)
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS expenses(
+            id INTEGER PRIMARY KEY,
+            date TEXT,
+            payee TEXT,
+            amount REAL,
+            memo REAL
+            )
+        """)
+    
+    # cur.execute("""
+    #             INSERT INTO expenses VALUES (1, '7/11/2025', 'GameStop', 69.99, 'Metroid Prime 4')
+    #             """)
+    # Get most recent id:
+    res = cur.execute("SELECT id FROM expenses")
+    
+    
+    
+    
+    
+    
+    actions = {
+        "1": "Add a transaction",
+        "2": "Update a transaction"
+    }
 
+    for choice, desc in actions.items():
+        print(f"{choice}. {desc}")
+        
+    action = input("What would you like to do? (Select Number): ")
 
+    if action in actions:
+        match action:
+            case "1": # Adding transaction
+                print(7)    
+            case _:
+                print("Not currently supported")
+                con.close()
+                return
+    else:
+        con.close()
+        return
+        
 
-con.close()
+# match hello:
+#     case "yes":
+#         x = 7
+#     case _:
+#         x = 8
+        
+# print(x)
+
+    con.close()
+    
+if __name__ == "__main__":
+    meyno_basic()
