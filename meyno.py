f85ba7d3002bb9efbd207554b9754a28ea5a4597
ChaoticDefense <supersonicseven@gmail.com>
2025-07-10 21:07:18 -0500

    first actual commit working on this. just added basic opening of a sqlite3 database with .meyno extension
    Add .meyno files to .gitignore

---
+++ meyno.py
@@ -0,0 +1,14 @@
+# This is the day that the LORD has made. Let us rejoice and be glad in it.
+# Psalm 118:24
+
+import os
+import sqlite3
+from pathlib import Path
+
+# Global Variables
+SCRIPT_DIR = Path(__file__).parent
+
+# Makes a database called budget.meyno (if it doesn't exist) and connects to it
+con = sqlite3.connect("budget.meyno")
+
+con.close()
