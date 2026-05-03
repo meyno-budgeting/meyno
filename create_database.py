524aa39d848722a7bd9f6bda3379f01237d08668
ChaoticDefense <supersonicseven@gmail.com>
2026-05-02 20:59:37 -0500

    made table classes for foundation tables

--- create_database.py
+++ create_database.py
@@ -1,6 +1,8 @@
 from typing import List, Optional
+from decimal import Decimal
 
-from sqlalchemy import String, ForeignKey, create_engine
+from datetime import date
+from sqlalchemy import String, Date, Numeric, ForeignKey, create_engine, text
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
@@ -10,7 +12,7 @@ class User(Base):
     __tablename__ = "user"
     
     user_id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String)
+    email: Mapped[str] = mapped_column(String, unique=True)
     password: Mapped[str] = mapped_column(String) # TODO: Make this a hashed value or something
     full_name: Mapped[Optional[str]] = mapped_column(String)
     
@@ -26,6 +28,29 @@ class Account(Base):
     
     owner: Mapped["User"] = relationship(back_populates="accounts")
     
+class Payee(Base):
+    __tablename__ = "payee"
+    
+    payee_id: Mapped[int] = mapped_column(primary_key=True)
+    payee_name: Mapped[str] = mapped_column(String, unique=True)
+    
+class Category(Base):
+    __tablename__ = "category"
+    
+    category_id: Mapped[int] = mapped_column(primary_key=True)
+    category_name: Mapped[str] = mapped_column(String, unique=True)
+    
+class Transaction(Base):
+    __tablename__ = "transaction"
+
+    transaction_id: Mapped[int] = mapped_column(primary_key=True)
+    account_id: Mapped[int] = mapped_column(ForeignKey("account.account_id"), nullable=False)
+    transaction_date: Mapped[date] = mapped_column(Date)
+    payee_id: Mapped[int] = mapped_column(ForeignKey("payee.payee_id"))
+    category_id: Mapped[int] = mapped_column(ForeignKey("category.category_id"))
+    amount: Mapped[Decimal] = mapped_column(Numeric(precision=10, scale=2), nullable=False, default=Decimal("0.00"), server_default=text("0.00"))
+
+
 engine = create_engine("sqlite:///:memory:", echo=True)
 
 Base.metadata.create_all(engine)
