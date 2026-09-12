4ef3d10d24159370492fd113cb975619a32621c3
ChaoticDefense <supersonicseven@gmail.com>
2026-09-11 21:05:23 -0500

    added separate test for previewing Python 3.15

--- noxfile.py
+++ noxfile.py
@@ -8,3 +8,18 @@ def tests(session: nox.Session) -> None:
     session.install(".")
     session.install("pytest", "pytest-cov")
     session.run("pytest")
+
+
+@nox.session(python="3.15")
+def tests_315(session: nox.Session) -> None:
+    session.env["PYTHONPATH"] = "src"
+
+    session.install(
+        "alembic>=1.20,<1.21",
+        "pydantic>=2.13.0,<3.0",
+        "sqlalchemy>=2.0,<2.1",
+        "pytest>=9.0,<10.0",
+        "pytest-cov>=7.1,<8.0",
+    )
+
+    session.run("pytest")
